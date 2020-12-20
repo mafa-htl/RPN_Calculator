c44ea09073c9136eb8b051912796e0c6c043d08e
Matteo Falkenberg <matteo.falkenberg@htl-wels.at>
2020-12-20 15:00:05 +0100

    graphical overlay

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("calc.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("RPN Calculator");
+        primaryStage.setScene(new Scene(root, 400, 400));
         primaryStage.show();
     }
 
